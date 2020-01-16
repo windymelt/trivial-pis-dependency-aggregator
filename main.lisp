69dceb7bd06dbcb060a9b2628eb9ee9990736584
Windymelt <windymelt@hatena.ne.jp>
2020-01-16 11:18:00 +0900

    commit

---
+++ main.lisp
@@ -0,0 +1,17 @@
+(in-package :cl-user)
+(defpackage :trivial-pis-dependency-aggregator
+  (:use :cl)
+  (:import-from :alexandria :make-keyword)
+  (:export :define-dependency-aggregator-package :*asdf-pathname*))
+
+(defparameter *asdf-pathname* "Specify :PATHNAME of your system e.g. #P\"src/\"." nil) ;; e.g. #P"src/"
+(defparameter *dependency-source-dir* (merge-pathnames *asdf-path* (uiop:getcwd)))
+(defun define-dependency-aggregator-package (name)
+  "Defines package which has dependency to packages inside directory named NAME. Directory \"NAME\" should be placed aside of caller of this function."
+  (let ((file-names-without-ext
+          (mapcar #'pathname-name
+                  (uiop:directory-files
+                   (merge-pathnames (pathname (format nil "~A/" (string-downcase (subseq (symbol-name name) (1+ (position #\/ (symbol-name name) :from-end t)))))) *dependency-source-dir*)))))
+    `(defpackage ,name
+       (:use :cl)
+       ,@(mapcar #'(lambda (f) (list :import-from (make-keyword (string-upcase (format nil "~A/~A" name f))))) file-names-without-ext))))
---
+++ trivial-pis-dependency-aggregator.asd
@@ -0,0 +1,25 @@
+(in-package :cl-user)
+(defpackage trivial-pis-dependency-aggregator-asd
+  (:use :cl :asdf))
+(in-package :trivial-pis-dependency-aggregator-asd)
+
+(defsystem trivial-pis-dependency-aggregator
+  :version "0.1"
+  :author "Windymelt"
+  :license "MIT"
+  :depends-on (:alexandria)
+  :components ((:file "main"))
+  :description ""
+  :long-description
+  #.(with-open-file (stream (merge-pathnames
+                             #p"README.md"
+                             (or *load-pathname* *compile-file-pathname*))
+                            :if-does-not-exist nil
+                            :direction :input)
+      (when stream
+        (let ((seq (make-array (file-length stream)
+                               :element-type 'character
+                               :fill-pointer t)))
+          (setf (fill-pointer seq) (read-sequence seq stream))
+          seq)))
+  )
