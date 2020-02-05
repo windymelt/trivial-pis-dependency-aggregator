aa51988da024123eba4d321426081ab4ed021390
Windymelt <windymelt@hatena.ne.jp>
2020-02-05 13:07:02 +0900

    修正

--- main.lisp
+++ main.lisp
@@ -2,16 +2,20 @@
 (defpackage :trivial-pis-dependency-aggregator
   (:use :cl)
   (:import-from :alexandria :make-keyword)
-  (:export :define-dependency-aggregator-package :*asdf-pathname*))
+  (:export :define-dependency-aggregator-package))
 
-(defparameter *asdf-pathname* "Specify :PATHNAME of your system e.g. #P\"src/\"." nil) ;; e.g. #P"src/"
-(defparameter *dependency-source-dir* (merge-pathnames *asdf-path* (uiop:getcwd)))
+(in-package :trivial-pis-dependency-aggregator)
+
+(defparameter *asdf-pathname-value* #P"./"
+  "Specify :PATHNAME of your system e.g. #P\"src/\".")
+
+(defun dependency-source-dir () (merge-pathnames trivial-pis-dependency-aggregator::*asdf-pathname-value* (uiop:getcwd)))
 (defun define-dependency-aggregator-package (name)
   "Defines package which has dependency to packages inside directory named NAME. Directory \"NAME\" should be placed aside of caller of this function."
   (let ((file-names-without-ext
           (mapcar #'pathname-name
                   (uiop:directory-files
-                   (merge-pathnames (pathname (format nil "~A/" (string-downcase (subseq (symbol-name name) (1+ (position #\/ (symbol-name name) :from-end t)))))) *dependency-source-dir*)))))
+                   (merge-pathnames (pathname (format nil "~A/" (string-downcase (subseq (symbol-name name) (1+ (position #\/ (symbol-name name) :from-end t)))))) (trivial-pis-dependency-aggregator::dependency-source-dir))))))
     `(defpackage ,name
        (:use :cl)
        ,@(mapcar #'(lambda (f) (list :import-from (make-keyword (string-upcase (format nil "~A/~A" name f))))) file-names-without-ext))))
